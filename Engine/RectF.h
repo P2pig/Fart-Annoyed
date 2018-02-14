e95b49f426103defafbaaa52f8ff701049909524
babysean_ <sean791022@gmail.com>
2018-02-14 16:52:09 +1300

    Added RectF

---
+++ Engine/RectF.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Vec2.h"
+
+class RectF
+{
+public:
+	RectF() = default;
+	RectF( float left, float right, float top, float bottom );
+	RectF( Vec2 topLeft, Vec2 bottomRight );
+	RectF( Vec2 topLeft, float width, float height );
+	bool IsColliding( const RectF& other ) const;
+public:
+	float top;
+	float bottom;
+	float left;
+	float right;
+};
