0c3990c40b98261ab2b3adff3db0d21b36f5a8f1
babysean_ <sean791022@gmail.com>
2018-02-15 15:14:57 +1300

    renamed RectF function isOverLapping

--- Engine/RectF.h
+++ Engine/RectF.h
@@ -9,7 +9,7 @@ public:
 	RectF( float left, float top, float right, float bottom );
 	RectF( Vec2 topLeft, Vec2 bottomRight );
 	RectF( Vec2 topLeft, float width, float height );
-	bool IsColliding( const RectF& other ) const;
+	bool isOverLapping( const RectF& other ) const;
 	static RectF FromCenter( Vec2 center, float halfWidth, float halfHeight);
 public:
 	float top;
